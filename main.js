54ffc78185187a0ed0ba41da2204fa1aa450a2e3
Korean Przemek <przemyslaw.satro@gmail.com>
2022-01-28 19:16:18 +0100

    6.Class & style binding 7. Computed Properties. To be continued... (Prosze mnie nie inwigilowac bo tez w ryj dac moge dac ;)).

--- main.js
+++ main.js
@@ -1,35 +1,56 @@
 var app = new Vue ({
     el: '#app',
     data: {
+        whatsFirst: 'data silniejszy!',
+        brand: 'Vue Mastery',
         product: 'Socks',
-        inStock: true,
         onSale: true,
         details: ["80% cotton", "20% polyester", "Gender-neutral"],
-        image: './assets/vmSocks-green-onWhite.jpg',
+        selectedVariant: 0,
         variants: [{
             variantId: "2234",
-            variantColor: "green",
-            variantImage: './assets/bartek_kciuk.jpg'
+            backgroundColor: "blue",
+            variantImage: '.\\assets\\vmSocks-blue-onWhite.jpg',
+            variantQantity: 10,
         }, {
             variantId: "2235",
-            variantColor: "blue",
-            variantImage: './assets/bartek_oczy.jpg'
+            backgroundColor: "green",
+            variantImage: '.\\assets\\vmSocks-green-onWhite.jpg',
+            variantQantity: 0,
         }],
         sizes: ["35-38", "39-42",  "43-46"],
         cart: 0,
-
     },
     methods: {
         addToCart: function () {
             this.cart += 1;
         },
         subFromCart: function () {
-            if(this.cart === 0) return;
+            if(this.cart === 0) return; 
             this.cart -= 1;
+            return;
         },
-        updateProduct: function (variantImage) {
-            this.image = variantImage;
-        },
-        
+        updateProduct: function (index) {
+            this.selectedVariant = index;
+            return;
+        },  
     },
+    computed: {
+        whatsFirst() {
+            return 'Computed, proste'
+        },
+        title() {
+            return this.brand + ' ' + this.product;
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].variantQantity
+        },
+        comutedOnSale() {
+            return this.onSale ? `${this.brand} ${this.product} ${this.onSale}`: 'Actually not on sale.'
+        },
+
+    }
 })
